0d2bcfa155e6e27bf74fc48e6684a4a2a3cd2cf4
Stephen Sayage <stephensayage@gmail.com>
2020-09-01 15:27:43 -0400

    routes and controllers updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :likes
   resources :comments
   resources :posts
-  resources :tasks
   resources :users
+
+  post '/auth/login', to: 'auth#login'
+  get '/auth/verify', to: 'auth#verify'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
